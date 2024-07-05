0a39dfaa3383d9c2c2417f66ec7085b6b3983ff2
farrelzzzzzzz <farrelbalapradhana18@gmail.com>
2024-07-05 18:16:26 +0700

    Update Proses.php

--- Proses.php
+++ Proses.php
@@ -1,15 +1,19 @@
 <?php
-//inisialisasi dari form html 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nama = $_POST['nama'];
     $email = $_POST['email'];
     $ticket = $_POST['ticket'];
 
-    // Menyimpan data ke dalam file teks
+    
     $file = 'pesanan.txt';
-    $current = file_get_contents($file);
-    $current .= "Nama: $nama, Email: $email, Jumlah Tiket: $ticket\n";
-    file_put_contents($file, $current);
+    $handle = fopen($file, 'a'); 
+
+    
+    $data = "Nama: $nama, Email: $email, Jumlah Tiket: $ticket\n";
+    fwrite($handle, $data);
+
+    
+    fclose($handle);
 
     echo "Pemesanan tiket berhasil!";
 }
